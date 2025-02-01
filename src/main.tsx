56a8237e4435c2b939599c3999201aa1f8375a71
Marija Jovicic <marijajovicic65@gmail.com>
2025-02-01 01:09:03 -0600

    Cleanup

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,9 @@
 import ReactDOM from 'react-dom/client';
+import React from 'react';
+import App from "./App.tsx";
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
+ReactDOM.createRoot(document.getElementById('app')!).render(
     <React.StrictMode>
-        </React.StrictMode>,
+            <App/>
+    </React.StrictMode>
 );
