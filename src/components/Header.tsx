e78cbd19d56e5a80eda105eee4142e50c4830682
Marija Jovicic <marijajovicic65@gmail.com>
2025-02-01 12:41:21 -0600

    Adding datepicker in header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,7 +1,27 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { DesktopDatePicker } from '@mui/x-date-pickers/DesktopDatePicker';
+import { AdapterDateFns } from '@mui/x-date-pickers/AdapterDateFns';
+import { LocalizationProvider } from '@mui/x-date-pickers/LocalizationProvider';
 
-const Header: React.FC = () => {
-  return <header></header>;
+const Header = () => {
+  const [selectedDate, setSelectedDate] = useState<Date | null>(null);
+
+  const handleDateChange = (newDate: Date | null) => {
+    setSelectedDate(newDate);
+  };
+
+  return (
+    <header style={{ backgroundColor: '#f0f0f0' }}>
+      <LocalizationProvider dateAdapter={AdapterDateFns}>
+        <DesktopDatePicker
+          label="Izaberite datum"
+          format="dd.MM.yyyy"
+          value={selectedDate}
+          onChange={handleDateChange}
+        />
+      </LocalizationProvider>
+    </header>
+  );
 };
 
 export default Header;
